61cfd8ee87b778e2d03800b606a17bb32eebbad5
Ari Shashivkopanazak <ari.shashivkopanazak@gmail.com>
2019-06-18 16:53:45 -0700

    commit

--- src/main/java/frc/robot/RobotMap.java
+++ src/main/java/frc/robot/RobotMap.java
@@ -6,7 +6,7 @@ public class RobotMap {
   public final static int OI_XBOX = 0;
   public final static int OI_BUTTONBOARD = 1;
   //CargoIntake Constants
-  public final static int PWM_ID_SPARK_CARGO = 0; //I don't know what these ports are
-  public final static int DIO_LIMIT_CARGO = 1; //or what way they are plugged in
+  public final static int PWM_ID_SPARK_HAND = 0; //I don't know what these ports are
+  public final static int DIO_LIMIT_HAND = 1; //or what way they are plugged in
 
 }
