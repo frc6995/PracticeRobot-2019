a13f3b2b1091668a0b4845c2e5c13b675f720665
Ari Shashivkopanazak <ari.shashivkopanazak@gmail.com>
2019-07-03 14:10:44 -0700

    made "smart buttons"

--- src/main/java/frc/robot/OI.java
+++ src/main/java/frc/robot/OI.java
@@ -2,8 +2,10 @@ package frc.robot;
 
 import frc.robot.buttons.Xbox;
 import frc.robot.buttons.BBoard;
+import frc.robot.commands.arm.*;
 import frc.robot.commands.hand.CargoIntakeC;
 import frc.robot.commands.hand.CargoJettisonC;
+import edu.wpi.first.wpilibj.command.Subsystem;
 import frc.robot.RobotMap;
 
 public class OI {
@@ -14,7 +16,38 @@ public class OI {
 
   public OI() {
     //Button Board Commands
+    //hand
     buttonBoard.right_index_toggleOnPress(new CargoIntakeC());
     buttonBoard.right_index_toggleOnPress(new CargoJettisonC());
+
+
+    /*
+     encoder constants, for untested code
+     Ship = 1000
+     Rocket = 500
+     Home = 0
+    */
+    //home button
+    if (getEncoderCount <= 1000 && getEncoderCount >= 500){
+      buttonBoard.left_ring_runOnPress(new ArmShipToHome());
+    } else {
+      buttonBoard.left_ring_runOnPress(new ArmRocketToHome());
+    }
+
+    //rocket button, if above Rocket level, run Down PID, if below, run Up PID
+    if (getEncoderCount < 500) {
+      buttonBoard.left_middle_runOnPress(new ArmHomeToRocket());
+    } else {
+      buttonBoard.left_middle_runOnPress(new ArmShipToRocket());
+    }
+
+    //ship level
+    if (getEncoderCount >= 0 && getEncoderCount >= 500){
+      buttonBoard.left_index_runOnPress(new ArmHomeToShip());
+    } else {
+      buttonBoard.left_index_runOnPress(new ArmRocketToShip());
+    }
+    
+
   }
 }
