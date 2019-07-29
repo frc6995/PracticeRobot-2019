a98884d360e8d14934a080c03a4e7029e3794ead
Ari Shashivkopanazak <ari.shashivkopanazak@gmail.com>
2019-07-28 19:04:51 -0700

    renamed "hand" to "claw"

--- src/main/java/frc/robot/commands/hand/CargoIntakeC.java
+++ src/main/java/frc/robot/commands/hand/CargoIntakeC.java
@@ -8,7 +8,7 @@ public class CargoIntakeC extends Command {
   private double intakeSpeed = -0.7; //negative values intake?
 
   public CargoIntakeC() {
-    requires(Robot.m_CargoHandS);
+    requires(Robot.m_CargoClawS);
   }
 
   @Override
@@ -18,17 +18,17 @@ public class CargoIntakeC extends Command {
 
   @Override
   protected void execute() {
-    Robot.m_CargoHandS.cargoSpeed(intakeSpeed);
+    Robot.m_CargoClawS.cargoSpeed(intakeSpeed);
   }
 
   @Override
   protected boolean isFinished() {
-    return Robot.m_CargoHandS.getCargoLimit() || isTimedOut();
+    return Robot.m_CargoClawS.getCargoLimit() || isTimedOut();
   }
 
   @Override
   protected void end() {
-    Robot.m_CargoHandS.cargoSpeed(0.0);
+    Robot.m_CargoClawS.cargoSpeed(0.0);
   }
 
   @Override
